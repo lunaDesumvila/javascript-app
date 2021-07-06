f58d2154002b29f4c45e45fe34755f2dbfd7847c
luna desumvila <lunitadesumvila@hotmail.com>
2021-07-06 10:24:13 -0300

    tod

---
+++ js/arrays.js
@@ -0,0 +1,129 @@
+//////////////////// ARRAY VERDURAS //
+const verduras = [{
+        id: 1,
+        producto: "TOMATE",
+        precio: 140,
+        img: "./assets/img/tomate.jpg",
+        seleccionado: false
+    },
+    {
+        id: 2,
+        producto: "LECHUGA",
+        precio: 110,
+        img: "./assets/img/lechuga.png",
+        seleccionado: false
+
+    },
+    {
+        id: 3,
+        producto: "ZANAHORIA",
+        precio: 80,
+        img: "./assets/img/zanahoriaa.jpg",
+        seleccionado: false
+
+    },
+    {
+        id: 4,
+        producto: "REPOLLO",
+        precio: 85,
+        img: "./assets/img/repollo.jpg",
+        seleccionado: false
+
+    }, {
+        id: 5,
+        producto: "ZAPALLITO",
+        precio: 100,
+        img: "./assets/img/zapallito.jpg",
+        seleccionado: false
+
+    },
+    {
+        id: 6,
+        producto: "BERENJENA",
+        precio: 96,
+        img: "./assets/img/berenjena.jpg",
+        seleccionado: false
+
+    }, {
+        id: 7,
+        producto: "CEBOLLA",
+        precio: 70,
+        img: "./assets/img/cebolla.jpg",
+        seleccionado: false
+
+    },
+    {
+        id: 8,
+        producto: "PIMIENTO AMARRILLO",
+        precio: 280,
+        img: "./assets/img/pimiento-amarrillo.jpg",
+        seleccionado: false
+
+    }
+];
+console.log(verduras)
+
+//////////////////// ARRAY FRUTAS //
+const frutas = [{
+        id: 11,
+        producto: "SANDIA",
+        precio: 140,
+        img: "./assets/img/sandia.jpg",
+        seleccionado: false
+    },
+    {
+        id: 12,
+        producto: "BANANA",
+        precio: 110,
+        img: "./assets/img/banana.jpg",
+        seleccionado: false
+
+    },
+    {
+        id: 13,
+        producto: "MANZANA ROJA",
+        precio: 80,
+        img: "./assets/img/manzana.jpg",
+        seleccionado: false
+
+    },
+    {
+        id: 14,
+        producto: "NARANJA",
+        precio: 85,
+        img: "./assets/img/naranja.jpg",
+        seleccionado: false
+
+    }, {
+        id: 15,
+        producto: "FRUTILLA",
+        precio: 100,
+        img: "./assets/img/frutilla-180x180.jpg",
+        seleccionado: false
+
+    },
+    {
+        id: 16,
+        producto: "MANDARINA",
+        precio: 96,
+        img: "./assets/img/mandarina.jpg",
+        seleccionado: false
+
+    }, {
+        id: 17,
+        producto: "UVA",
+        precio: 70,
+        img: "./assets/img/uva.jpg",
+        seleccionado: false
+
+    },
+    {
+        id: 18,
+        producto: "MELON",
+        precio: 280,
+        img: "./assets/img/melon.png",
+        seleccionado: false
+
+    }
+];
+console.log(frutas)
